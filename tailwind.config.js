b121e13d1d3a242a92f5a771f9c7c8a6ad66275c
pomelott <pomelott@163.com>
2021-02-08 17:57:48 +0800

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.jsx'
+  ],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
